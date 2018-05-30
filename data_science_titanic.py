f1d4757156bd55a44fab6d68a436281d6a0184aa
roney-amorim <roney.augusto.amorim@gmail.com>
2018-05-30 01:42:34 -0300

    ajustes entrega

--- data_science_titanic.py
+++ data_science_titanic.py
@@ -7,14 +7,106 @@ import random
 import pandas as pandas
 import numpy as np
 
-# Vamos ler os dados como uma lista
+# Lendo os dados como uma lista
 print("Lendo o documento...")
 with open("titanic-data-6.csv", "r") as file_read:
     data_list = [{k: v for k, v in row.items()} 
         for row in csv.DictReader(file_read, skipinitialspace=True)]
-print(data_list)
+#print(data_list)
 
+total_passageiros = len(data_list)
+print(total_passageiros)
 
+#########################################################################################################
+
+def count_people_by_class(data_list):
+    '''
+      Função que retorno a quantidade de pessoas de gêneros masculino e feminino identificada no data_list.
+      Argumentos:
+          data_list: Arquivo no formato de dictionary.
+      Retorna:
+          Quantidade de pessoas de gêneros masculino e feminino.
+    '''
+    first = 0
+    second = 0
+    third = 0
+
+    for item in data_list:
+        if item['Pclass'].title() == '1':
+            first += 1
+        elif item['Pclass'].title() == '2':
+            second += 1
+        elif item['Pclass'].title() == '3':
+            third += 1
+    return [first, second,third]
+
+peoples = count_people_by_class(data_list)
+print(peoples)
+
+def count_people_by_class2(data_list):
+    '''
+      Função que retorno a quantidade de pessoas de gêneros masculino e feminino identificada no data_list.
+      Argumentos:
+          data_list: Arquivo no formato de dictionary.
+      Retorna:
+          Quantidade de pessoas de gêneros masculino e feminino.
+    '''
+    first = 0
+    second = 0
+    third = 0
+
+    for item in data_list:
+        if item['Pclass'].title() == '1' and item['Survived'].title() == '1':
+            first += 1
+        elif item['Pclass'].title() == '2' and item['Survived'].title() == '1':
+            second += 1
+        elif item['Pclass'].title() == '3' and item['Survived'].title() == '1':
+            third += 1
+    return [first, second,third]
+
+peoples2 = count_people_by_class2(data_list)
+print(peoples2)
+
+#########################################################################################################
+
+def count_people_survived(data_list):
+    '''
+      Função que retorno a quantidade de pessoas de gêneros masculino e feminino identificada no data_list.
+      Argumentos:
+          data_list: Arquivo no formato de dictionary.
+      Retorna:
+          Quantidade de pessoas de gêneros masculino e feminino.
+    '''
+    survived = 0
+    dead = 0
+    for item in data_list:
+        if item['Survived'].title() == '1':
+            survived += 1
+        if item['Survived'].title() == '0':
+            dead += 1
+        
+    return [survived,dead]
+
+print(count_people_survived(data_list))
+
+'''
+def count_people_by_class(classe):
+    Função que retorno a quantidade de pessoas de gêneros masculino e feminino identificada no data_list.
+    Argumentos:
+        data_list: Arquivo no formato de dictionary.
+    Retorna:
+        Quantidade de pessoas de gêneros masculino e feminino.
+    count_peoples = 0
+    for item in data_list:
+        if item['Pclass'].title() == classe:
+            count_peoples += 1
+
+    return count_peoples
+
+first = count_people_by_class('1')
+second = count_people_by_class('2')
+third = count_people_by_class('3')
+'''
 #Quantas pessoas pertenciam a 1º classe
 #Quantas pessoas pertenciam a 2º classe
 #Quantas pessoas pertenciam a 3º classe  
